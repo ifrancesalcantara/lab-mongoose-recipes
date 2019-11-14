f2e48106adfd1d61467971d506ca80193fc6ff0f
Ivan <ifrancesalcantara@gmail.com>
2019-11-14 20:43:35 +0100

    [BCN-FTWD-2019] Iván-Francés

--- index.js
+++ index.js
@@ -10,3 +10,53 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  const mySchema = {
+    title: "Troltilla",
+    level: "UltraPro Chef",
+    ingredients: ["troll", "tila"], // would this be the same as {type: Array}?
+    cuisine: "Yes",
+    dishType: "Other",
+    image: "http://img.desmotivaciones.es/201101/trolltilladepatatas.jpg",
+    duration: 225,
+    creator: "Ivan",
+    created: new Date()
+  }
+var trolltilla = Recipe.create(mySchema)
+console.log(mySchema.title)
+
+Recipe.insertMany(data)
+data.forEach(element => {
+  console.log(element.title);
+});
+
+Recipe.updateOne({ title: "Rigatoni alla Genovese" }, {$set:{ duration: 100 }})
+.then(result=>{
+  console.log("Succeded updating one element")
+})
+.catch(err=>{
+  console.log(err)
+})
+
+Recipe.deleteOne({ title: 'Carrot Cake' }, ()=>{}) //It is not actually deleted, I think
+.then(result=>{
+  console.log("Succeded deleting one element")
+})
+.catch(err=>{
+  console.log(err)
+})
+
+
+mongoose.connection.on('connected', () => console.log('Mongoose connected'));
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', () => console.log('Mongoose disconnected'));
+
+// If the connection throws an error
+mongoose.connection.on('error', (err) => console.log('Mongoose connection error: ' + err));
+
+process.on('SIGINT', () => {   //this is not running and "mongoose.connection.close()" throws error
+  mongoose.connection.close(() => { 
+    console.log('Mongoose connection disconnected through app termination'); 
+    process.exit(0); 
+  }); 
+});
